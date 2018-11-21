d32638c0c04ec129d13bdc0085381f03221c0684
AlynxZhou <alynx.zhou@gmail.com>
2018-11-21 23:12:51 +0800

    Updated indention.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const coffee = require("coffeescript")
 
 module.exports = (hikaru) => {
-	const coffeeConfig = hikaru.site.get("siteConfig")["coffeescript"] || {}
+  const coffeeConfig = hikaru.site.get("siteConfig")["coffeescript"] || {}
   hikaru.renderer.register(".coffee", ".js", (file, ctx) => {
     file["content"] = coffee.compile(file["text"], coffeeConfig)
     return file
